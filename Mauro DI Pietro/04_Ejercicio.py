abc234a8476449b53c8b1bb42edfd6d1122c72ff
dipietromauro9-create <dipietromauro9@gmail.com>
2025-09-09 11:47:29 -0300

    ejercicio 4 CORREGIDO

--- Mauro DI Pietro/04_Ejercicio.py
+++ Mauro DI Pietro/04_Ejercicio.py
@@ -1,6 +1,4 @@
-lista_numeros=[]
-for i in range (5):
-    numero=int(input("Ingrese un numero "))
-    lista_numeros.append(numero)
+lista=(input("Ingrese una lista de numeros seperadas por espacios: "))
+lista_numeros=[int(x)for x in lista.split()]
 pares=[x for x in lista_numeros if x %2==0]
 print(f"La cantidad de pares en la ista son {len(pares)}")
