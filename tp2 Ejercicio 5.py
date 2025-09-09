d54fc1029f3919fe4b06012d7560eb4c5ab238ba
francosampieri <francosampiee@gmail.com>
2025-09-09 12:45:30 -0300

    cambio nombre

---
+++ tp2 Ejercicio 5.py
@@ -0,0 +1,17 @@
+<<<<<<< HEAD
+#Escribe un programa que encuentre el valor más grande en una lista bidimensional.
+lista_bidimensional = [[0,1,8],[0,190,4],[100,-5,21]]
+mayor = max(max(lista_bidimensional[0]), max(lista_bidimensional[1]), max(lista_bidimensional[2]))
+print(mayor)
+=======
+numeros = [2 , 4 ,6 ,8 , 10]
+
+factor = int(input("INGRESE UN FACTOR PARA MULTIPLICAR LOS NUMEROS DE LA LISTA : "))
+
+
+resultado = [num*factor for num in numeros ]
+
+
+print("Lista Original " , numeros )
+print("Lista Multiplicada " , resultado )
+>>>>>>> d4409fb37346e1d230fcd38498749477aa23c343
